080a5da3e275f3043179dcec95ea0ab14f387edf
whiteand <andrewbeletskiy@gmail.com>
2024-12-02 19:08:17 +0200

    feat: solved second part of the day 2

--- y24/d02/part2/src/main.zig
+++ y24/d02/part2/src/main.zig
@@ -19,7 +19,51 @@ pub fn main() !void {
 }
 
 fn solve(file_content: []const u8, allocator: std.mem.Allocator) !usize {
-    std.debug.print("allocator: {any}\n", .{allocator});
-    std.debug.print("file_content:\n{s}\n", .{file_content});
-    return 0;
+    var it = std.mem.splitSequence(u8, file_content, "\n");
+    var total: usize = 0;
+    var nums = try std.ArrayList(usize).initCapacity(allocator, 8);
+    defer nums.deinit();
+
+    while (it.next()) |line| {
+        if (line.len == 0) {
+            continue;
+        }
+        nums.clearRetainingCapacity();
+        var nums_strs = std.mem.splitScalar(u8, line, ' ');
+        while (nums_strs.next()) |num_str| {
+            const num = try std.fmt.parseInt(usize, num_str, 10);
+            try nums.append(num);
+        }
+        if (is_safe(nums.items)) {
+            total += 1;
+        } else {
+            for (0..nums.items.len) |i| {
+                const value = nums.orderedRemove(i);
+                if (is_safe(nums.items)) {
+                    total += 1;
+                    break;
+                }
+                try nums.insert(i, value);
+            }
+        }
+    }
+
+    return total;
+}
+
+fn is_safe(items: []usize) bool {
+    var sign: i8 = 0;
+    for (items[0..(items.len - 1)], items[1..]) |a, b| {
+        if (a == b) {
+            return false;
+        }
+        if (a > b and (sign == -1 or a - b > 3)) {
+            return false;
+        }
+        if (a < b and (sign == 1 or b - a > 3)) {
+            return false;
+        }
+        sign = if (a > b) 1 else -1;
+    }
+    return true;
 }
